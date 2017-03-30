a9155f75ccfbc50e2c5ab05df427a775ce95576a
Jason Britton <bjaon1@vt.edu>
2017-03-30 15:38:06 -0400

    Return only posts that have not been checked previously, added option to log to file (not finished)

--- alertme.py
+++ alertme.py
@@ -1,17 +1,26 @@
 import argparse
 import requests
 import time
+from datetime import datetime
+
+most_recent_post = ""
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-s', required=True, help='subreddit to search')
-parser.add_argument('-t', help='search terms')
+parser.add_argument('-t', help='search terms separated by +')
+parser.add_argument('-l', action='store_true', help='create log file')
 args = parser.parse_args()
 
 subreddit = args.s
 terms = args.t
+log = args.l
 
 
 def search(s, t):
+    global most_recent_post
+
+    print(datetime.now().strftime('%Y-%m-%d %H:%M:%S'), end=' ')
+
     if not t:
         t = ""
 
@@ -22,7 +31,18 @@ def search(s, t):
     if r.status_code == requests.codes.ok:
         data = r.json()
         for post in data['data']['children']:
+            # Check if posts have already been seen
+            if post['data']['title'] == most_recent_post:
+                break
+            # Alert user
             print(post['data']['title'])
+
+        # Update most_recent_post to the most recent post in data
+        if data['data']['children']:
+            most_recent_post = data['data']['children'][0]['data']['title']
+            print("most_recent_post", most_recent_post)
+        else:
+            print("most_recent_post not updated")
 
         time.sleep(60)
     else:
@@ -31,6 +51,9 @@ def search(s, t):
         except requests.exceptions.HTTPError:
             print("Error: status code", r.status_code)
             time.sleep(5)
+
+if log:
+    file = open(datetime.now().strftime('%Y-%m-%d_%H-%M-%S') + ".txt", 'w')
 
 while True:
     search(subreddit, terms)
