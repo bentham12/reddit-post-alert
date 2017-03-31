83c7392a14340aa17470aca35675675244f9777f
bentham12 <bjason1@vt.edu>
2017-03-31 00:08:31 -0400

    Finished logging functionality

--- alertme.py
+++ alertme.py
@@ -19,7 +19,7 @@ log = args.l
 def search(s, t):
     global most_recent_post
 
-    print(datetime.now().strftime('%Y-%m-%d %H:%M:%S'), end=' ')
+    log_out(datetime.now().strftime('%Y-%m-%d %H:%M:%S') + "\nChecking for new posts...\n")
 
     if not t:
         t = ""
@@ -35,25 +35,34 @@ def search(s, t):
             if post['data']['title'] == most_recent_post:
                 break
             # Alert user
-            print(post['data']['title'])
+            log_out(str(post['data']['title']) + "\n")
 
         # Update most_recent_post to the most recent post in data
         if data['data']['children']:
             most_recent_post = data['data']['children'][0]['data']['title']
-            print("most_recent_post", most_recent_post)
-        else:
-            print("most_recent_post not updated")
 
-        time.sleep(60)
+        log_out("\n")
+        time.sleep(300)
     else:
         try:
             r.raise_for_status()
         except requests.exceptions.HTTPError:
-            print("Error: status code", r.status_code)
-            time.sleep(5)
+            log_out("Error: status code " + str(r.status_code) + "\n\n")
+            time.sleep(20)
+
+
+def log_out(message):
+    print(message, end='')
+    if log:
+        log_file = open(log_file_name, 'a')
+        try:
+            log_file.write(message)
+        except:
+            log_file.write("Error writing message to file")
+        log_file.close()
 
 if log:
-    file = open(datetime.now().strftime('%Y-%m-%d_%H-%M-%S') + ".txt", 'w')
+    log_file_name = datetime.now().strftime('%Y-%m-%d_%H-%M-%S') + ".txt"
 
 while True:
     search(subreddit, terms)
