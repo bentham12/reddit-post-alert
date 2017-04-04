3d6f01928922906ac192995c00253bbc6df02fcb
Jason Britton <bjaon1@vt.edu>
2017-04-04 14:54:05 -0400

    Added text message alerts

--- alertme.py
+++ alertme.py
@@ -1,16 +1,24 @@
 import argparse
 import requests
 import time
+import smtplib
 from datetime import datetime
 
 most_recent_post = ""
 
 parser = argparse.ArgumentParser()
+parser.add_argument('-n', required=True, help='10-digit phone number with email gateway (ex. 1234567890@txt.att.net) '
+                                              'to receive text alert')
+parser.add_argument('-e', required=True, help='email address of GMail bot account to send text from')
+parser.add_argument('-p', required=True, help='bot account password')
 parser.add_argument('-s', required=True, help='subreddit to search')
 parser.add_argument('-t', help='search terms separated by +')
 parser.add_argument('-l', action='store_true', help='create log file')
 args = parser.parse_args()
 
+phone_number = args.n
+email = args.e
+password = args.p
 subreddit = args.s
 terms = args.t
 log = args.l
@@ -40,7 +48,9 @@ def search(s, t):
             if post['data']['title'] == most_recent_post:
                 break
             # Alert user
-            log_out(str(post['data']['title']) + "\n")
+            message = str(post['data']['title']) + "\n"
+            log_out(message)
+            alert(message)
 
         # Update most_recent_post to the most recent post in data
         if data['data']['children']:
@@ -54,6 +64,19 @@ def search(s, t):
         except requests.exceptions.HTTPError:
             log_out("Error: status code " + str(r.status_code) + "\n\n")
             time.sleep(20)
+
+
+def alert(message):
+    global email, phone_number, password
+    log_out("Sending alert\n")
+    try:
+        server = smtplib.SMTP('smtp.gmail.com:587')
+        server.starttls()
+        server.login(email, password)
+        server.sendmail(email, phone_number, message)
+        server.quit()
+    except:
+        log_out("Error sending alert\n")
 
 
 def log_out(message):
